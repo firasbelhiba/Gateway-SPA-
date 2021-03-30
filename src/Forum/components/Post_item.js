964a78705ad8deef18b1bf771673ccac5daa0f78
ghadakhedri1 <61046136+ghadakhedri1@users.noreply.github.com>
2021-03-30 20:49:52 +0100

    edit form+navbar

--- src/Forum/components/Post_item.js
+++ src/Forum/components/Post_item.js
@@ -18,7 +18,7 @@ const Post_item = () => {
             <a href="#" title="" class="ed-opts-open">
               <i class="la la-ellipsis-v"></i>
             </a>
-            <ul class="ed-options">
+            <ul class="ed-options  post_project">
               <li>
                 <a href="#" title="">
                   Edit Post

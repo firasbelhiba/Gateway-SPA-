15901b2a44ff87613acf23424e795dc431405fd8
firasbelhiba <firas.belhiba@esprit.tn>
2021-04-23 00:57:24 +0100

    Pagination

--- src/settings/components/Notification.js
+++ src/settings/components/Notification.js
@@ -1,9 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import TimeAgo from "timeago-react";
+import Pagination from "../../Shared/layouts/Pagination";
 
 
 const Notification = () => {
     let this_profile = JSON.parse(localStorage.getItem("profile"));
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(5);
+
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentNotifications = this_profile.notification.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
         <Fragment>
@@ -15,22 +28,32 @@ const Notification = () => {
             >
                 <div className="acc-setting">
                     <h3>Notifications</h3>
-                    {this_profile.notification.map(p => (
-                        <div className="notifications-list">
 
-                            <div className="notfication-details">
-                                <div className="noty-user-img">
-                                    <img src="images/resources/ny-img1.png" alt="" />
-                                </div>
-                                <div className="notification-info">
-                                    <h3>
-                                        {p.message}
-                                    </h3>
-                                    <span><TimeAgo datetime={p.date} locale="vi" /></span>
+                    {currentNotifications.map(p => (
+                        <Fragment>
+
+                            <div className="notifications-list">
+
+                                <div className="notfication-details">
+                                    <div className="noty-user-img">
+                                        <img src="images/resources/ny-img1.png" alt="" />
+                                    </div>
+                                    <div className="notification-info">
+                                        <h3>
+                                            {p.message}
+                                        </h3>
+                                        <span><TimeAgo datetime={p.date} locale="vi" /></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </Fragment>
+
                     ))}
+                    <Pagination
+                        postsPerPage={postsPerPage}
+                        totalPosts={this_profile.notification.length}
+                        paginate={paginate}
+                    />
 
                 </div>
             </div>
